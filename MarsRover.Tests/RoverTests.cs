3e34c4b2f62a24b2058289339d5f8e8abef49a23
jonathanbull <jonathanbull@gmail.com>
2014-03-19 17:52:44 +0000

    Add test SetUp of rover object. Add skeleton rover object.

---
+++ MarsRover.Tests/RoverTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Tests
+{
+    [TestFixture]
+    public class RoverTests
+    {
+        IRover iRover;
+
+        [SetUp]
+        public void SetUp()
+        {
+            iRover = new Rover();
+        }
+    }
+}
